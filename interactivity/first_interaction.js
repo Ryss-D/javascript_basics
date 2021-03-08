542773ad4caa783643ad42f9b98c10d5404f2cd3
unknown <LENOVO@CMU-M3DM.Priscila.local>
2021-03-08 10:52:00 -0500

    that the firs content learning js

---
+++ interactivity/first_interaction.js
@@ -0,0 +1,7 @@
+// Javascript doesnbt have a built in print fuction but we can
+
+window.alert();//Alert box
+window.prompt();//prompt prompt act as a input variable
+document.write();//HTML output
+element.innerHTML = "things";//HTML element
+console.log();//the browser console
---
+++ interactivity/vars.js
@@ -0,0 +1,11 @@
+var name;
+// we have to declare the bariables
+
+var name = "Colleen";
+
+window.prompt();//prompt prompt act as a input variable
+//we can werite
+var name = window.prompt("whats your name?")
+
+//data types
+//js dont diference floats forom ints
